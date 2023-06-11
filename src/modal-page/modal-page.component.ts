581bfda983641fdafd52e12ffeec21c6e42a1d54
pressi10 <121279313+pressi10@users.noreply.github.com>
2023-06-11 11:52:05 +0530

    2 files added

---
+++ src/modal-page/modal-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-modal-page',
+  templateUrl: './modal-page.component.html',
+  styleUrls: ['./modal-page.component.css']
+})
+export class ModalPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
